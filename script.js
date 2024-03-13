794963c85b25f31f5bcd53993cde827c9521e02a
virginboy6417 <olajide1adebayo2@gmail.com>
2024-03-13 10:57:23 +0100

    AAA

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ function checkscore() {
     if (score.value >=70 && score.value <=100){
         result.innerHTML="A-EXCELLENT"
     }
+    else if (score.value ==""){
+        result.innerHTML = "input can not be empty"
+            }
     else if(score.value >=60 && score.value <=69){
-        result.innerHTML="B-Very Good"
+        result.innerHTML="B-Good"
     }
     else if(score.value >=50 && score.value <=59){
         result.innerHTML="CREDIT"
@@ -14,18 +17,17 @@ function checkscore() {
         result.innerHTML="D-Pass"
     }
     else if (score.value >=40 && score.value <=44){
-       result.innerHTML="FAIL"
+       result.innerHTML="E"
   
     }
-    else if (score.value >=0 && score.value <=39){
-        failure.play()
-        result.innerHTML="YOU ARE FAILURE"
-    }
-    else if (score.value ==""){
-        result.innerHTML="input can not be empty"
-    }
+
+else if(score.value >=0 && score.value <=39){
+result.innerHTML = "FAIL"
+failure.play()
+}
     else {
-        result.innerHTML="Wrong Input"
+        result.innerHTML ="Wrong input"
+        
     }
     
 }
